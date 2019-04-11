fb5c041c8f27193d3debd5827225af4e85af4c09
nbstsh <nbstsh@gmail.com>
2019-04-11 20:31:35 +0900

    Create route to get log

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const app = express()
 const logger = require('./log/logger')
+const fs = require('fs')
 
 
+process.on('unhandledRejection', (ex) => {
+    console.log(ex)
+})
+
+process.on('uncaughtException', (ex) => {
+    console.log(ex)
+})
 
 app.get('/', (req, res) => {
     res.send('Weolcome to line bot sample!!!')
 })
 
+app.get('/logs', (req, res) => {
+    fs.readFile('logs/combined.log', (err, data) => {
+        if (err)  return res.status(500).send(err)
+
+        const dataStrArray = data.toString().split('\n')
+        dataStrArray.pop()
+        const dataJsonArray = dataStrArray.map(str => JSON.parse(str))
+
+        res.send(dataJsonArray)
+    })
+})
+
+
 app.post('/webhook', (req, res) => {
-    logger.info('post /webhook')
 
     res.send('webhook')
 })
--- src/log/logger.js
+++ src/log/logger.js
@@ -2,6 +2,7 @@ const { createLogger, transports, format } = require('winston')
 
 
 const logger = createLogger({
+    format: format.json(),
     transports: [
         new transports.File({ filename: 'logs/error.log', level: 'error' }),
         new transports.File({ filename: 'logs/combined.log' }) 
